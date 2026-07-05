6c55b78eb637cbc7827bf6cbd15591fca9cc0bf4
Artatrana Sahu <artatranasahu89@gmail.com>
2026-07-06 00:24:13 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,71 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
+import Home from "./Home";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
- 
-  const login = async () => {
-    try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        {
-          email,
-          password,
-        }
-      );
-
-      localStorage.setItem("token", res.data.data.token);
-
-      localStorage.setItem(
-        "user",
-        JSON.stringify(res.data.data.user)
-      );
-
-      alert("Login Successful!");
-      window.location.href = "/dashboard";
-    
-    } catch (error) {
-      alert("Login Failed");
-      console.error(error);
-    }
-  };
- 
-
-  return (
-    <div
-      style={{
-        maxWidth: "400px",
-        margin: "50px auto",
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-      }}
-    >
-      <h1>STRIDE Login</h1>
-
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) =>
-          setEmail(e.target.value)
-        }
-      />
-
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) =>
-          setPassword(e.target.value)
-        }
-      />
-
-      <button onClick={login}>
-        Login
-      </button>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
